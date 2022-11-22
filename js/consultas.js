b4ec68d2c90396ba4c562352d9d1e099158d6c06
pilarmolina <pili.molina@live.com.ar>
2022-11-21 22:33:23 -0300

    Se implementan mejoras en el formulario de consultas de javascript

--- js/consultas.js
+++ js/consultas.js
@@ -1,34 +1,52 @@
-
+ 
 //Creo la clase para las consultas
 class consulta {
-    constructor (nombre, correo, mensaje) {
+    constructor (nombre, correo, mensaje, numeroMensaje) {
         this.nombre = nombre;
         this.correo = correo;
         this.mensaje = mensaje; 
+        this.numeroMensaje = numeroMensaje;
     }
 }
 
-const consulta1 = new consulta("Marta Perez" , "martaperez12345@gmail.com", "quiero saber cuanto sale el hospedaje para dos personas");
-const consulta2 = new consulta("Alberto Gomez" , "albertogomez12345@gmail.com", "hola, quiero saber si tienen cabañas para 20 personas");
+//Creo array 
 
+let arrayConsulta = []
 
-// Creo el array y hago el push con las consultas
-const arrayConsulta = []
-
-arrayConsulta.push (consulta1);
-arrayConsulta.push (consulta2);
-
-console.log (arrayConsulta)
-
-// Agrego la consulta que hace el usuario al array 
-
-function hacerConsulta () {
-    let nombreyapellido = prompt ("Ingrese su nombre y apellido");
-    let correo = prompt ("Ingrese su correo electrónico");
-    let mensaje = prompt ("Ingrese su consulta");
-    let nuevaConsulta = new consulta (nombreyapellido,correo,mensaje);
-    arrayConsulta.push (nuevaConsulta);
+if (localStorage.getItem("arrayConsulta")) {
+    arrayConsulta = JSON.parse(localStorage.getItem("arrayConsulta"));
 }
 
-hacerConsulta();
-console.log(arrayConsulta)
+//Creo variable contadora
+
+let contador = JSON.parse(localStorage.getItem("contador"));
+
+//Obtengo datos del formulario 
+
+const formulario = document.getElementById("formularioConsulta");
+
+formularioConsulta.addEventListener ("submit", (e) => {
+    e.preventDefault ();
+    hacerConsulta ()
+})
+
+function hacerConsulta () {
+    const nombreyapellido = document.getElementById ("nombre").value;
+    const correo = document.getElementById ("correo").value;
+    const mensaje =  document.getElementById ("mensaje").value;
+    if ((nombreyapellido.lenth == 0) || (correo.lenth == 0) || (mensaje.lenth == 0)) {
+        alert ("Todos los campos deben estar completos");
+     }
+     else {   
+        const numeroMensaje = contador++;
+        const nuevaConsulta = new consulta (nombreyapellido,correo,mensaje,numeroMensaje);
+        arrayConsulta.push (nuevaConsulta);
+        formulario.reset ();
+        numeroConsulta.innerHTML= `${numeroMensaje}`;   
+        localStorage.setItem ("arrayConsulta", JSON.stringify(arrayConsulta));
+        localStorage.setItem ("contador", JSON.stringyfy (contador));
+    }
+}
+
+console.log(arrayConsulta);
+
