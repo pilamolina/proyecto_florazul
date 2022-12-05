64b8e5bfadec5b491a83eb777150380ce873b950
pilarmolina <pili.molina@live.com.ar>
2022-12-05 09:31:14 -0300

    Se agrega el envío de consultas al mail entre otros ajustes de javascript

--- js/consultas.js
+++ js/consultas.js
@@ -1,11 +1,11 @@
- 
+
 //Creo la clase para las consultas
 class consulta {
-    constructor (nombre, correo, mensaje, numeroMensaje) {
-        this.nombre = nombre;
+    constructor (nombreyapellido, correo, mensaje, contador) {
+        this.nombreyapellido = nombreyapellido;
         this.correo = correo;
         this.mensaje = mensaje; 
-        this.numeroMensaje = numeroMensaje;
+        this.contador = contador;
     }
 }
 
@@ -33,28 +33,48 @@ formularioConsulta.addEventListener ("submit", (e) => {
 let mensajeFormulario = document.getElementById("mensajeFormulario");
 
 function hacerConsulta () {
-    const nombreyapellido = document.getElementById("nombre").value;
+    const nombreyapellido = document.getElementById("nombreyapellido").value;
     const correo = document.getElementById("correo").value;
     const mensaje =  document.getElementById("mensaje").value;
     if ((nombreyapellido.length == 0) || (correo.length == 0) || (mensaje.length == 0)) {
         mensajeFormulario.innerHTML = `
         Por favor complete todos los campos
         `
-     }
-     else {   
+    }
+    else {   
         contador++;
         const nuevaConsulta = new consulta (nombreyapellido,correo,mensaje,contador);
         arrayConsulta.push(nuevaConsulta);
         formulario.reset ();
         /*let numeroConsulta = document.getElementById("numeroConsulta");
         numeroConsulta.innerHTML= `${contador}`;   */
-        mensajeFormulario.innerHTML = `
-        Su número de consulta es: ${contador}. Responderemos su consulta lo antes posible.
-        `
-        localStorage.setItem ("arrayConsulta", JSON.stringify(arrayConsulta));
-        localStorage.setItem ("contador", JSON.stringify (contador));
+        const serviceID = "service_c25cawd";
+        const templateID = "template_0k5n10r";
+        emailjs
+        .send(serviceID,templateID,nuevaConsulta)
+        .then((res) => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Tu consulta fue enviada con éxito',
+                confirmButtonColor: '#5998C5',
+                text: 'El número de consulta es: + ${contador}'
+            })
+            /*mensajeFormulario.innerHTML = `
+            Su número de consulta es: ${contador}. Responderemos su consulta lo antes posible.
+            ` */
+            console.log(res);
+            localStorage.setItem ("arrayConsulta", JSON.stringify(arrayConsulta));
+            localStorage.setItem ("contador", JSON.stringify (contador));
+        })
+        .catch ((err) => 
+            Swal.fire({
+                icon: 'error',
+                title: 'Lo sentimos, hubo un error',
+                confirmButtonColor: '#5998C5',
+                text: 'Por favor vuelva a intentarlo mas tarde o envíe un correo con su consulta a florazulbariloche@gmail.com mientras resolvemos este inconveniente'
+            })
+        );
     }
 }
-
 console.log(arrayConsulta);
 
